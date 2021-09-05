0c482d1d841af42cbcf5bd070e9a13b9e0ea07f9
AricZhou <z260952274@gmail.com>
2021-09-05 12:11:22 +0800

    Finish the API

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use rocket::{catch,catcher,delete,get,put,post,routes};
+use rocket::serde::json::{Value};
+use rocket::serde::json::serde_json::json;
+use rocket::{catch, catchers, delete, get, post, put, routes};
 
 
 #[get("/")]
@@ -6,7 +8,35 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/")]
+async fn get_products() -> Value{
+    json!("list")
+}
 
+#[get("/<id>")]
+async fn view_products(id:i32) -> Value {
+    json!("get it!")
+}
+
+#[post("/")]
+async fn create_products() -> Value {
+    json!("create")
+}
+
+#[post("/<id>")]
+async fn put_products(id:i32) -> Value {
+    json!("put")
+}
+
+#[delete("/<id>")]
+async fn delete_products(id:i32) -> Value {
+    json!("delete")
+}
+
+#[catch(404)]
+async fn not_found_url() -> Value {
+    json!("Not Found")
+}
 
 #[rocket::main]
 async fn main() -> Result<(),Box<dyn std::error::Error>> {
@@ -18,6 +48,7 @@ async fn main() -> Result<(),Box<dyn std::error::Error>> {
             create_products,
             put_products,
             delete_products])
+        .register("/", catchers!(not_found_url))
     .launch().await?;
     Ok(())
 }
